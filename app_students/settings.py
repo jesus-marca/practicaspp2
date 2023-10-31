acf5154d8c8116fcb8ab0b4c2ec5b7e4d4d33a3b
braulio-marca <braulio.marca@unmsm.edu.pe>
2023-10-30 23:48:11 -0500

    correccion de estilos principal y secundario

--- app_students/settings.py
+++ app_students/settings.py
@@ -163,7 +163,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
